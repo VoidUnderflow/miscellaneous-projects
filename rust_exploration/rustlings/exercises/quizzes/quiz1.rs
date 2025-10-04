ea789c5fa8ef023f8ca63413656d92246db49001
Mihai <alexmihai.ciobanu@gmail.com>
2025-10-04 11:41:16 +0100

    [Rustlings] Add if exercises and quiz1 solution.

--- rust_exploration/rustlings/exercises/quizzes/quiz1.rs
+++ rust_exploration/rustlings/exercises/quizzes/quiz1.rs
@@ -8,9 +8,12 @@
 // - However, if Mary buys more than 40 apples, the price of each apple in the
 // entire order is reduced to only 1 rustbuck!
 
-// TODO: Write a function that calculates the price of an order of apples given
-// the quantity bought.
-// fn calculate_price_of_apples(???) -> ??? { ??? }
+fn calculate_price_of_apples(apple_quantity: i32) -> i32 {
+    if apple_quantity <= 40 {
+        return 2 * apple_quantity;
+    }
+    apple_quantity
+}
 
 fn main() {
     // You can optionally experiment here.
