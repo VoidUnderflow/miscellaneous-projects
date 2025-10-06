7ca8ba43147bd83b4d73b1f97be03a4c109bd44a
Mihai <alexmihai.ciobanu@gmail.com>
2025-10-06 20:03:40 +0100

    [Rustlings] Add string exercises.

--- rust_exploration/rustlings/exercises/09_strings/strings4.rs
+++ rust_exploration/rustlings/exercises/09_strings/strings4.rs
@@ -9,29 +9,22 @@ fn string(arg: String) {
     println!("{arg}");
 }
 
-// TODO: Here are a bunch of values - some are `String`, some are `&str`.
-// Your task is to replace `placeholder(…)` with either `string_slice(…)`
-// or `string(…)` depending on what you think each value is.
 fn main() {
-    placeholder("blue");
+    string_slice("blue");
 
-    placeholder("red".to_string());
+    string("red".to_string());
+    string(String::from("hi"));
+    string("rust is fun!".to_owned());
 
-    placeholder(String::from("hi"));
+    string_slice("nice weather".into());
 
-    placeholder("rust is fun!".to_owned());
-
-    placeholder("nice weather".into());
-
-    placeholder(format!("Interpolation {}", "Station"));
+    string(format!("Interpolation {}", "Station"));
 
     // WARNING: This is byte indexing, not character indexing.
     // Character indexing can be done using `s.chars().nth(INDEX)`.
-    placeholder(&String::from("abc")[0..1]);
+    string_slice(&String::from("abc")[0..1]);
+    string_slice("  hello there ".trim());
 
-    placeholder("  hello there ".trim());
-
-    placeholder("Happy Monday!".replace("Mon", "Tues"));
-
-    placeholder("mY sHiFt KeY iS sTiCkY".to_lowercase());
+    string("Happy Monday!".replace("Mon", "Tues"));
+    string("mY sHiFt KeY iS sTiCkY".to_lowercase());
 }
