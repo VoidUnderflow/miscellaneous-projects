1ac2d5767780358efa5689b876970e64ec1eb523
Mihai <alexmihai.ciobanu@gmail.com>
2025-10-14 22:01:53 +0100

    [Rustlings] Add conversion functions, finish Rustlings!

--- rust_exploration/rustlings/exercises/23_conversions/from_str.rs
+++ rust_exploration/rustlings/exercises/23_conversions/from_str.rs
@@ -25,11 +25,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// TODO: Complete this `FromStr` implementation to be able to parse a `Person`
-// out of a string in the form of "Mark,20".
-// Note that you'll need to parse the age component into a `u8` with something
-// like `"4".parse::<u8>()`.
-//
 // Steps:
 // 1. Split the given string on the commas present in it.
 // 2. If the split operation returns less or more than 2 elements, return the
@@ -41,7 +36,27 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {}
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let strings: Vec<&str> = s.split(',').collect();
+
+        if strings.len() != 2 {
+            return Err(ParsePersonError::BadLen);
+        }
+
+        if strings[0].is_empty() {
+            return Err(ParsePersonError::NoName);
+        }
+
+        let age: Result<u8, ParseIntError> = strings[1].parse::<u8>();
+        match age {
+            Ok(val) => {
+                Ok(Person {name: strings[0].to_string(), age: val})
+            },
+            Err(err) => {
+                Err(ParsePersonError::ParseInt(err))
+            }
+        }
+    }
 }
 
 fn main() {
--- rust_exploration/rustlings/exercises/23_conversions/try_from_into.rs
+++ rust_exploration/rustlings/exercises/23_conversions/try_from_into.rs
@@ -23,27 +23,53 @@ enum IntoColorError {
     IntConversion,
 }
 
-// TODO: Tuple implementation.
 // Correct RGB color values must be integers in the 0..=255 range.
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
 
-    fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {}
+    fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        for val in [tuple.0, tuple.1, tuple.2] {
+            if !(0..255).contains(&val) {
+                return Err(IntoColorError::IntConversion);
+            }
+        }
+
+        Ok(Color {red: tuple.0 as u8, green: tuple.1 as u8, blue: tuple.2 as u8})
+    }
 }
 
-// TODO: Array implementation.
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
 
-    fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {}
+    fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        let to_u8 = |x: i16| u8::try_from(x).map_err(|_| IntoColorError::IntConversion);
+
+        Ok(Color {
+            red: to_u8(arr[0])?,
+            green: to_u8(arr[1])?,
+            blue: to_u8(arr[2])?
+        })
+    }
 }
 
-// TODO: Slice implementation.
 // This implementation needs to check the slice length.
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
 
-    fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {}
+    fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3 {
+            return Err(IntoColorError::BadLen);
+        }
+
+        let to_u8 = |x: i16| u8::try_from(x).map_err(|_| IntoColorError::IntConversion);
+
+        Ok(Color {
+            red: to_u8(slice[0])?,
+            green: to_u8(slice[1])?,
+            blue: to_u8(slice[2])?
+        })
+        
+    }
 }
 
 fn main() {
