aa07280a9525041147abfe574f2211a033beafaa
Mihai <alexmihai.ciobanu@gmail.com>
2026-01-18 17:56:58 +0000

    [Node] Add a logEvents function.

--- node_exploration/server.ts
+++ node_exploration/server.ts
@@ -11,6 +11,8 @@
 // console.log(add(2, 3));
 
 import fs from "fs";
+import { logEvents } from "./logEvents.js";
+
 fs.readFile("./dummy.txt", "utf-8", (err, data) => {
   if (err) throw err;
   console.log(data);
@@ -22,3 +24,5 @@ process.on("uncaughtException", (err) => {
   console.error(`There was an uncaught error: ${err}`);
   process.exit(1);
 });
+
+await logEvents("Message here - where will it be saved?");
