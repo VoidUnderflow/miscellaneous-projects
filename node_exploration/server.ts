4fc1a6e889950f14d2539c0edaa1dcd33f5da159
Mihai <alexmihai.ciobanu@gmail.com>
2026-01-18 15:41:32 +0000

    [Node] Read a file.

--- node_exploration/server.ts
+++ node_exploration/server.ts
@@ -1,11 +1,24 @@
-import { add } from "./math.js";
+// import { add } from "./math.js";
 
-console.log("Hello World");
-console.log(global);
+// console.log("Hello World");
+// console.log(global);
 
-import os from "os";
-console.log(os.type());
-console.log(os.version());
-console.log(os.homedir());
+// import os from "os";
+// console.log(os.type());
+// console.log(os.version());
+// console.log(os.homedir());
 
-console.log(add(2, 3));
+// console.log(add(2, 3));
+
+import fs from "fs";
+fs.readFile("./dummy.txt", "utf-8", (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
+
+console.log("Hello...");
+
+process.on("uncaughtException", (err) => {
+  console.error(`There was an uncaught error: ${err}`);
+  process.exit(1);
+});
