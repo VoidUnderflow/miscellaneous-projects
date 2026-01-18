a939c6fea11b8de14931bdeac75880be7c8b80d7
Mihai <alexmihai.ciobanu@gmail.com>
2026-01-18 15:35:37 +0000

    [Node] Add module and import some functions.

--- node_exploration/server.ts
+++ node_exploration/server.ts
@@ -1,3 +1,5 @@
+import { add } from "./math.js";
+
 console.log("Hello World");
 console.log(global);
 
@@ -5,3 +7,5 @@ import os from "os";
 console.log(os.type());
 console.log(os.version());
 console.log(os.homedir());
+
+console.log(add(2, 3));
